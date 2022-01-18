8d283f8b0e8c97c61279632358120580a6844cc8
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2022-01-17 23:30:25 -0300

    agora vai

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const decodify = require('./scripts/decoder')
 const app = express()
-
+const port = process.env.PORT || 3000
 app.use(require("cors")())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
@@ -25,6 +25,8 @@ app.get('/retornaCodigos', (req, res) =>{
 
 })
  
-var server = http.createServer(app); 
-server.listen(3001);
+
+app.listen(port, () =>{
+        console.info(`Aplicação rodando na porta ${port}!`)
+});
 console.log("Servidor escutando na porta 3030...")
